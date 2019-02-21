11d543859fc90cb01d9a3b03b3fd04387f5b35b4
Darcy Linde <lindedarcy@gmail.com>
2019-02-21 03:41:38 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,5 +15,10 @@
 def speak_to_grandma(message)
   if message  == 'I LOVE YOU GRANDMA!'
     'I LOVE YOU TOO PUMPKIN!'
-  elsif message == upcase.
+  elsif message == message.upcase
+    'NO, NOT SINCE 1938!'
+  else
+    'HUH?! SPEAK UP, SONNY!'
+  end
+  
 end
